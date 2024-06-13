2ec48cba6d81f61364e5a49e07d1e26037efd3af
Iwai Koei <kd1305362@st.kobedenshi.ac.jp>
2024-06-13 14:12:58 +0900

    メイン画面

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Story Teller</title>
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <div class="header-container">
+        <header>
+            <img src="sttera.png" alt="Story Teller" class="title-image">
+            <button class="menu-button" onclick="toggleSidebar()">メニュー</button>
+        </header>
+    </div>
+</body>
+
+</html>
