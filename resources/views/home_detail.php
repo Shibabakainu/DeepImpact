c13de1526a21e7fd40c52fce182e29bc3f5ef3a4
MIYOSHI YUDAI <kd1336726@st.kobedenshi.ac.jp>
2024-06-13 14:12:04 +0900

    作成画面一応

---
+++ resources/views/home_detail.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/home_detail.css">
+</head>
+
+<body>
+    <main>
+        <div class="main-container">
+            <p class="kotoba">合言葉</p>
+            <input class="textbox" type="text" name="pass" placeholder="半角数字1～4文字">
+            <a href="#"><button class="back">戻る</button></a>
+            <a href="#"><button class="create">作成</button></a>
+        </div>
+    </main>
+</body>
+
+</html>
