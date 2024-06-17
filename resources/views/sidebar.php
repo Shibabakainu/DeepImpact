ce4a32680d257fff4f4b91c4baa7b23bf45934d8
MIYOSHI YUDAI <kd1336726@st.kobedenshi.ac.jp>
2024-06-17 14:50:16 +0900

    sideberポップアップ追加

--- resources/views/sidebar.php
+++ resources/views/sidebar.php
@@ -1,9 +1,36 @@
 <div id="sidebar" class="sidebar" style="background-color: #fae6b1;">
     <ul>
         <li><a href="/deepimpact/resources/views/index.php">ホームに戻る</a></li>
-        <li><a href="rule.php">ルール</a></li>
+        <li><button id="sidebar-click-btn">ルール</button>
+            <div id="sidebar-popup-wrapper">
+                <div id="sidebar-popup-inside">
+                    <div id="sidebar-close">X</div>
+                    <div class="text2">
+                        <h2>Story Teller</h2>
+                        <p>ルールです。<br>改行</p>
+                    </div>
+                </div>
+            </div>
+        </li>
         <li><a href="/deepimpact/resources/views/login/profile.php">プロフィール</a></li>
         <li><a href="/deepimpact/resources/views/logout.php">ログアウト</a></li>
-    
     </ul>
 </div>
+
+<script>
+    const sidebarClickBtn = document.getElementById('sidebar-click-btn');
+    const sidebarPopupWrapper = document.getElementById('sidebar-popup-wrapper');
+    const sidebarClose = document.getElementById('sidebar-close');
+
+    // ボタンをクリックしたときにポップアップを表示させる
+    sidebarClickBtn.addEventListener('click', () => {
+        sidebarPopupWrapper.style.display = "block";
+    });
+
+    // ポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
+    sidebarPopupWrapper.addEventListener('click', e => {
+        if (e.target.id === sidebarPopupWrapper.id || e.target.id === sidebarClose.id) {
+            sidebarPopupWrapper.style.display = 'none';
+        }
+    });
+</script>
