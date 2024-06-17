fc5f548a76caededda4fba7499b3ea69d64a3055
Inoue Ryoutarou <kd1317823@st.kobedenshi.ac.jp>
2024-06-17 14:05:24 +0900

    game

---
+++ resources/views/game.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  <title>game</title>
+  <link rel="stylesheet" href="/deepimpact/resources/css/game.css">
+  </head>
+  <body>
+    
+<p>カードをホバーしてみてね</p>
+
+<ul>
+  <li><div class="card1"><img src="/deepimpact/images/art1.jpg"></div></li>
+  <li><div></div></li>
+  <li><div></div></li>
+  <li><div></div></li>
+  <li><div></div></li>
+  <li><div></div></li>
+</ul>
+
+  </body>
+</html>
+
+
+
