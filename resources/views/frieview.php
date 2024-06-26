9b9a874a35c9676215c6ab051f11fd258b9cf405
sssssshiba <trunghieu061220@gmail.com>
2024-06-26 12:11:29 +0900

    ヒエウ：frieview edit

--- resources/views/frieview.php
+++ resources/views/frieview.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>フレンド一覧画面</title>
-    <link rel="stylesheet" href="/deepimpact/resources/css/friend.css">
+    <link rel="stylesheet" href="/deepimpact/resources/css/frieview.css">
 </head>
 <body>
     <?php
+    session_start();
+    include 'header.php';
     // フレンドリストの配列
     $friends = ['フレンド名', 'フレンド名', 'フレンド名', 'フレンド名', 'フレンド名', 'フレンド名'];
     ?>
@@ -18,7 +20,7 @@
                 <div class="friend-item"><?php echo htmlspecialchars($friend, ENT_QUOTES, 'UTF-8'); ?></div>
             <?php endforeach; ?>
         </div>
-        <button class="friend-search">フレンド検索</button>
+        <button class="friend-search" onclick="location.href='/deepimpact/resources/views/friend.php'">フレンド検索</button>
         <button class="logout">退出</button>
     </div>
 </body>
