76c2b5996e76e4de029cbc3eae66387932f2acf9
tanaka marukusu <kd1262435@st.kobedenshi.ac.jp>
2024-07-05 11:23:02 +0900

    7/5 fixed confirmation.php :106 link

--- resources/views/login/confirmation.php
+++ resources/views/login/confirmation.php
@@ -103,7 +103,7 @@
                     // Get the last inserted user ID
                     $user_id = $stmt->insert_id;
                     // Redirect to the profile page with user ID
-                    header("Location: /DeepImpact/resources/login/login.php?id=$user_id");
+                    header("Location: /DeepImpact/resources/views/login/login.php?id=$user_id");
                     exit;
                 } else {
                     echo "Error: " . $stmt->error;
