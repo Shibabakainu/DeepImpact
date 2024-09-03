7f010fd7fd8bcd665085a9b2194b1dd457e6f028
tanaka marukusu <kd1262435@st.kobedenshi.ac.jp>
2024-09-03 10:14:38 +0900

    09/03 add ドロー

--- server/server.php
+++ server/server.php
@@ -4,104 +4,128 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
-class Game{
-    
-    private $state;
-    private $players;
-
-    public function __construct($clients)
-    {
-        $this->state ='waiting';
-        $this->players=[];
-    }
-
-    public function addPlayer($playerId){
-        $this->players[$playerId]=['score'=>0];
-    }
-    
-    public function removePlayer($playerId){
-        unset($this->players[$playerId]);
-    }
-    
-    public function startGame(){
-        $this->state = 'playing';
-        $this->broadcastState();
-    }
-    
-    public function endGame(){
-        $this->state ='ended';
-        $this->broadcastState();
-    }
-
-    public function broadcastState(){
-        $stateMessage = json_encode([
-            'type' => 'state',
-            'state' => $this->state,
-            'players' => $this->players
-        ]);
-
-        foreach ($this->clients as $client) {
-            $client->send($stateMessage);
-        }
-    }
-
-    public function handleAction($playerId,$action)
-    {
-        if($this->state !== 'playing')
-        {
-            return;
-        }
-        switch ($action['type']){
-            case 'draw_card';
-            $this->drawCard($playerId);
-            break;
-        }
-        $this->broadcastState();
-    }
-
-    private function drawCard($playerId){
-        if(count($this->deck)===0){
-            return;
-        }
-        $card = array_pop($this->deck);
-        $this->players[$playerId]['hand'][]= $card;
-        $this->players[$playerId]['score'] += $card; 
-    }
-}
-
-class Chat implements MessageComponentInterface {
+class StorytellerGame implements MessageComponentInterface {
     protected $clients;
+    protected $gameState;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->gameState = [
+            'players' => [],
+            'deck' => $this->initializeDeck(),
+            'currentRound' => [],
+            'votes' => []
+        ];
     }
 
     public function onOpen(ConnectionInterface $conn) {
-        // 新しいクライアントが接続したときに呼び出されます
         $this->clients->attach($conn);
         echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        // メッセージを受信したときに呼び出されます
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // 自分以外のすべてのクライアントにメッセージを送信
-                $client->send($msg);
-            }
+        $data = json_decode($msg, true);
+
+        switch ($data['type']) {
+            case 'join':
+                $this->handleJoin($from, $data['username']);
+                break;
+            case 'draw_card':
+                $this->handleDrawCard($from);
+                break;
+            case 'play_card':
+                $this->handlePlayCard($from, $data['card']);
+                break;
+            case 'vote':
+                $this->handleVote($from, $data['card']);
+                break;
         }
     }
 
     public function onClose(ConnectionInterface $conn) {
         // クライアントが切断したときに呼び出されます
+        unset($this->gameState['players'][$conn->resourceId]);
         $this->clients->detach($conn);
+        $this->broadcastPlayerList(); // プレイヤーリストを更新して送信
         echo "Connection {$conn->resourceId} has disconnected\n";
+
+        $this->broadcastPlayerList();
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
-        // エラーが発生したときに呼び出されます
         echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
+    }
+
+    protected function handleJoin(ConnectionInterface $conn, $username) {
+        // 新しいプレイヤーがゲームに参加
+        $this->gameState['players'][$conn->resourceId] = [
+            'username' => $username,
+            'hand' => []
+        ];
+        $this->broadcastPlayerList(); // プレイヤーリストを送信
+    }
+
+    protected function handleDrawCard(ConnectionInterface $conn) {
+        // プレイヤーがカードを引く
+        if (!empty($this->gameState['deck'])) {
+            $card = array_pop($this->gameState['deck']);
+            $this->gameState['players'][$conn->resourceId]['hand'][] = $card;
+            $conn->send(json_encode(['type' => 'update_hand', 'hand' => $this->gameState['players'][$conn->resourceId]['hand']]));
+        }
+    }
+
+    protected function handlePlayCard(ConnectionInterface $conn, $card) {
+        // プレイヤーがカードを出す
+        $this->gameState['currentRound'][$conn->resourceId] = $card;
+        if (count($this->gameState['currentRound']) == count($this->gameState['players'])) {
+            $this->broadcastGameState();
+        }
+    }
+
+    protected function handleVote(ConnectionInterface $conn, $card) {
+        // プレイヤーが投票する
+        $this->gameState['votes'][$conn->resourceId] = $card;
+        if (count($this->gameState['votes']) == count($this->gameState['players'])) {
+            $this->calculateResults();
+        }
+    }
+
+    protected function calculateResults() {
+        // 投票結果を計算する
+        $this->broadcastGameState();
+        // 次のラウンドの準備
+        $this->gameState['currentRound'] = [];
+        $this->gameState['votes'] = [];
+    }
+
+    protected function broadcastGameState() {
+        foreach ($this->clients as $client) {
+            $client->send(json_encode([
+                'type' => 'game_state',
+                'state' => $this->gameState
+            ]));
+        }
+    }
+
+    protected function broadcastPlayerList() {
+        // 参加しているプレイヤーのリストを全クライアントに送信
+        $playerList = [];
+        foreach ($this->gameState['players'] as $player) {
+            $playerList[] = $player['username'];
+        }
+
+        foreach ($this->clients as $client) {
+            $client->send(json_encode([
+                'type' => 'player_list',
+                'players' => $playerList
+            ]));
+        }
+    }
+
+    protected function initializeDeck() {
+        // デッキを初期化する
+        return range(1, 52); // 例として52枚のカードを使用
     }
 }
 
@@ -112,10 +136,11 @@ use Ratchet\WebSocket\WsServer;
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
-            new Chat()
+            new StorytellerGame()
         )
     ),
     8080
 );
 
 $server->run();
+?>
